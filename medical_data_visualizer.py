5ef4df06b3be550c11785358bf68230c15daeeb3
Manu-Remarti <lmanuel.reyes88@gmail.com>
2025-07-07 16:26:09 -0600

    Update medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -16,19 +16,27 @@ df['gluc'] = (df['gluc'] > 1).astype(int)
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
-
-    # 6
-    df_cat = None
+    df_cat = pd.melt(
+        df,
+        id_vars=['cardio'],
+        value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight']
+    )
     
-
+    # 6
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
+    
     # 7
 
 
-
     # 8
-    fig = None
+    fig = sns.catplot(
+        x='variable',
+        y='total',
+        hue='value',
+        col='cardio',
+        kind='bar',
+        data=df_cat
+    ).fig
 
 
     # 9
